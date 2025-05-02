a1e05a0da32b81964c7d494ae6828d09253aa0b1
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-05-02 16:54:15 -0300

    Meu primeiro serve com Node em typescirpt

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-console.log("Hello World");
-export {};
+import { createServer } from "node:http";
+const server = createServer((req, res) => {
+    console.log(req.method);
+    res.end("Hello World");
+});
+server.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
